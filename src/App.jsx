0b31738d103442922f9669ff605a8aff10b4b0be
AdamAbdulkareeem <dmabdulkareem@gmail.com>
2026-05-25 07:28:37 +0100

    Create App.js which the real working page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from "react";
+import { getTopCoins } from "./api"
 // import  './api/coinsService.js'
 
+
 export default function App(){
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const ctrl = new AbortController();
+  
+    getTopCoins({ perPage: 10, signal: ctrl.signal })
+    .then((data) => {
+      setCoins(data)
+    })
+    .catch((error) => {
+      if (ctrl.signal.aborted) return;
+      setError(error)
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+
+    return () => ctrl.abort();
+}, [])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Something went wrong: {error.message}</p>;
   return (
-    <h1></h1>
+    <ul>
+      {coins.map((coin) => (
+        <li key={coin.id}>
+          {coin.market_cap_rank}. {coin.name} ({coin.symbol.toUpperCase()} -- ${coin.current_price})
+        </li>
+      ))}
+    </ul>
   )
 }         
